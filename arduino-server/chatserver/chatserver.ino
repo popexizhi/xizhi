b2d2c7374b33b9ba000ac1eef6b7b83037b14339
popexizhi <popexizhi@gmail.com>
2014-07-24 09:56:39 +0800

    testgoble

--- arduino-server/chatserver/chatserver.ino
+++ arduino-server/chatserver/chatserver.ino
@@ -28,7 +28,7 @@ byte mac[] = {
 IPAddress ip(192,168,1, 177);
 IPAddress gateway(192,168,1, 1);
 IPAddress subnet(255, 255, 0, 0);
-
+const int _powerPin=13;    //the number for power pin  [add for popexihi]
 
 // telnet defaults to port 23
 EthernetServer server(23);
@@ -39,6 +39,8 @@ void setup() {
   Ethernet.begin(mac, ip, gateway, subnet);
   // start listening for clients
   server.begin();
+  //initialise the power pin as an output
+   pinMode(_powerPin,OUTPUT); 
  // Open serial communications and wait for port to open:
   Serial.begin(9600);
    while (!Serial) {
@@ -49,7 +51,16 @@ void setup() {
   Serial.print("Chat server address:");
   Serial.println(Ethernet.localIP());
 }
-
+void _Openpower(){
+ // add for popexizhi 
+ // open power
+digitalWrite(_powerPin,HIGH);
+}
+void _Closepower(){
+// add for popexizhi
+// close power
+digitalWrite(_powerPin,LOW);
+}
 void loop() {
   // wait for a new client:
   EthernetClient client = server.available();
@@ -60,15 +71,26 @@ void loop() {
       // clead out the input buffer:
       client.flush();    
       Serial.println("We have a new client");
-      client.println("Hello, client!"); 
+      client.println("Hello,arduino is ok!"); 
       alreadyConnected = true;
     } 
 
     if (client.available() > 0) {
       // read the bytes incoming from the client:
       char thisChar = client.read();
+      //String getcon=String("open");
+      String getcon = String(thisChar);
       // echo the bytes back to the client:
       server.write(thisChar);
+      //process thisChar.string
+      if (getcon== "open" ){
+      //open power
+      _Openpower();
+      }
+      if (getcon == "close"){
+     //close power
+      _Closepower();
+      }
       // echo the bytes to the server as well:
       Serial.write(thisChar);
     }

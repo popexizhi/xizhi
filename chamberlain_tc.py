62e761db51f8ba060db6228d088a05e0e760f1cc
popexizhi <popexizhi@gmail.com>
2014-07-31 16:24:44 +0800

    add testcase for chamberlain_tc/thread

--- chamberlain_tc.py
+++ chamberlain_tc.py
@@ -6,27 +6,56 @@ import os
 import subprocess
 from chamberlain import chamberlain
 import unittest
-import thread
+import threading
+class Dserver(threading.Thread):
+	def __init__(self,num,path):
+		threading.Thread.__init__(self)
+		self.thread_cont=num
+		self.path=path
+
+	def run(self): 
+		#Overwrite run() method
+		print "********************Demo server"
+		os.system("pwd")
+		os.system("python "+self.path)
+		self.thread_cont=self.thread_cont-1
+	 
+	def stop(self):
+		print "********************over Dserver"
+		
+
+
+
 class test_chamberlain(unittest.TestCase):
+
+
 	
 	def setUp(self):
-		"""start Demo Server """
-		dserver="testD\\Servertcp.py"
-		os.system("pwd")
-		arg="python "+dserver
-		thread.start_new_thread(subprocess.call,arg)
-		#os.system("python "+dserver)
+		pass
+
+	def tearDown(self):
+		pass
 	
 	def testcallpower(self):
 		"""test message to sever """
 		print "testcallpower"
-		pass
+		_testopen=chamberlain()
+		_testopen.callpower('o')
+		res=_testopen.showpower()
+		print res
+		
 
 	def testshowpower(self):
 		"test server recve message"
 		print "testshowpower"
-		pass
+		#_testopen=chamberlain()
+		#_testopen.callpower('c')
+		#print _testopen.showpower()
 
 
 if __name__=="__main__":
+	"""start Demo Server """
+	thread = Dserver(1,"testD\\Servertcp.py")
+	thread.start()
+	
 	unittest.main()

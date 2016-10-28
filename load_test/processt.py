994f8e22d4a6b4803c53fdcefcf7a17dcc67f70b
popexizhi <popexizhi@gmail.com>
2016-10-28 12:11:26 +0800

    add use_time second test pass

--- load_test/processt.py
+++ load_test/processt.py
@@ -62,9 +62,40 @@ def TPS(s_sta):
 
     return x_res, y_res
 
+def use_time_second(s_sta):
+    """
+    s_sta : {key[second] : array[list] }
+    eg:
+        475
+        [array([  4.75992000e+05,   2.00000000e+00,   1.33852000e+05,   1.96000000e+02])]
+		495
+		[array([  4.95140000e+05,   3.00000000e+00,   1.33852000e+05,   6.06000000e+02])]
+		485
+		[array([  4.85443000e+05,   1.40000000e+01,   1.33852000e+05,   6.37000000e+02]), 
+         array([  4.85633000e+05,   1.30000000e+01,   1.33852000e+05,   6.36000000e+02]), 
+         array([  4.85643000e+05,   1.00000000e+00,   1.33852000e+05,   6.35000000e+02]), 
+         array([  4.85999000e+05,   1.50000000e+01,   1.33852000e+05,	6.38000000e+02])]
+		455
+		[array([  4.55678000e+05,   5.00000000e+00,   1.33852000e+05,	3.87000000e+02])]
+    """
+    x_res = []
+    y_res = []
+    for sec in s_sta:
+        num = len(s_sta[sec])
+        sum_use_time = 0
+        for use_time in s_sta[sec]:
+            sum_use_time = sum_use_time + use_time[1]
+        #sum_use_time = s_sta[sec].sum(argsort)
+        arg_use_time  = sum_use_time / num 
+        print "%s : %d, %f" % (str(sec), sum_use_time, arg_use_time)
+        x_res.append(sec)
+        y_res.append(arg_use_time)
+
+    return x_res, y_res
 
 def test_sta_sec():
     list_data = numpy.zeros((8, 4))
+    # recevie_time, use_time, hostid, package_num
     list_data[0]=[485643, 1, 133852, 635]
     list_data[1]=[475992, 2, 133852, 196]
     list_data[2]=[495140, 3, 133852, 606]
@@ -89,6 +120,19 @@ def test_TPS():
     res = sta_sec(list_data)
     print TPS(res)
 
+def test_use_time_second():
+    list_data = numpy.zeros((8, 4))
+    list_data[0]=[485643, 1, 133852, 635]
+    list_data[1]=[475992, 2, 133852, 196]
+    list_data[2]=[495140, 3, 133852, 606]
+    list_data[3]=[435714, 4, 133852, 385]
+    list_data[4]=[455678, 5, 133852, 387]
+    list_data[5]=[485633, 13, 133852, 636]
+    list_data[6]=[485443, 14, 133852, 637]
+    list_data[7]=[485999, 15, 133852, 638]
+    res = sta_sec(list_data)
+    print use_time_second(res)
 
 #test_sta_sec()
-test_TPS()
+#test_TPS()
+test_use_time_second()

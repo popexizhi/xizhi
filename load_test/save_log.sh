ec02daaa672ff18db9578f93d7f03bae1ed3113b
popexizhi <popexizhi@gmail.com>
2016-11-07 15:14:05 +0800

    add save_log.sh

---
+++ load_test/save_log.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dir_p="backup_log"
+save_p="~/test"
+log_f=$1
+packet_number_per_second=$2
+echo "save log:${log_f} packet_number_per_second: ${packet_number_per_second}"
+mkdir ${dir_p}
+mv ${log_f} ${dir_p}/${log_f}_${packet_number_per_second}
+scp ${dir_p}/${log_f}_${packet_number_per_second} jenkins@192.168.1.99:${save_p}

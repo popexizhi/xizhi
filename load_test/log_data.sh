049533b424225f6fd9916e79ea403c48322beaaa
popexizhi <popexizhi@gmail.com>
2016-11-08 19:03:01 +0800

    log_data.sh up

--- load_test/log_data.sh
+++ load_test/log_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ue_log=$1
 echo ${ue_log}
-cat ${ue_log}| grep "PacketHeader"|sed 's/^.*Tcp Model Test recv time: \(\d*\)/\1/g'|sed 's/\([0-9][0-9]*\),content: PacketHeader{"packet_number":"\([0-9][0-9]*\)","time_stamp":"\([0-9][0-9]*\)"}/\1,\3,\2/g'>${ue_log}
+cat ${ue_log}| grep "PacketHeader"|sed 's/^.*Tcp Model Test recv time: \(\d*\)/\1/g'|sed 's/\([0-9][0-9]*\),content: PacketHeader{"packet_number":"\([0-9][0-9]*\)","time_stamp":"\([0-9][0-9]*\)"}/\1,\3,\2/g'>process/${ue_log}
 echo "********************************************"
-cat ${ue_log}
+cat process/${ue_log}

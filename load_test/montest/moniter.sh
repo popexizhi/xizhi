396758ada416fdf6acdb4cab3a7ab4cc5b91d2e2
popexizhi <popexizhi@gmail.com>
2016-11-10 11:46:58 +0800

    add moniter tar

--- load_test/montest/moniter.sh
+++ load_test/montest/moniter.sh
@@ -4,8 +4,12 @@ VALVE_NUM=3000 #max
 CORE_NUM=1 #core num
 
 dev_path="/home/jenkins/test/process" #检查路径
+log_path="/home/jenkins/test" #检查路径
+log_back="/data/nocloadtest" #备份log路径
 core_path="/home/slim/test/dev_provision/provision_ue_use"
 do_path="/home/lijie/test/xizhi/load_test" #运行路径
+
+
 
 send_mail()
 {
@@ -13,7 +17,10 @@ send_mail()
         cat $1
         python sendMail.py $1
 }
-
+backup_log()
+{
+    tar -cvzf ${log_back}/$1_log.tar.gz ${log_path}/*log.txt* --remove-files 
+}
 do_ana()
 {   
     now_d=`date +%Y%m%d_%H%M%S`
@@ -21,6 +28,7 @@ do_ana()
     mv ${dev_path} ${dev_path}_${now_d}
     mkdir ${dev_path}
     chmod 777 ${dev_path}
+    backup_log ${now_d}
     echo "">nohup.out
     cd ${do_path} && python analysis_data.py ${dev_path}_${now_d} 
     
--- load_test/montest/resta_mon.sh
+++ load_test/montest/resta_mon.sh
@@ -3,7 +3,7 @@ for pid in $(ps aux|grep moniter.sh  |grep -v grep|awk '{print $2}');do
     echo Stop moniter.sh , killing pid: $pid
         kill -9 $pid
 done
-ehco "backup nohup"
+echo "backup nohup"
 tar -cvzf nohup.tar.gz nohup.out  --remove-files 
 ps -ef | egrep moniter.sh | egrep -v egrep
 

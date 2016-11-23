5a049a69d0f7ff092392616cd60649853c5236c1
popexizhi <popexizhi@gmail.com>
2016-11-23 16:56:45 +0800

    ana_moniter.sh mv

--- load_test/montest/resta_mon.sh
+++ load_test/montest/resta_mon.sh
@@ -1,5 +1,5 @@
 
-for pid in $(ps aux|grep "moniter.sh\|ana_moniter.sh" |grep -v grep|awk '{print $2}');do
+for pid in $(ps aux|grep "moniter.sh\|MoniterStart" |grep -v grep|awk '{print $2}');do
     echo Stop moniter.sh , killing pid: $pid
         kill -9 $pid
 done
@@ -11,6 +11,5 @@ sleep 2
 
 echo "start moniter.sh"
 nohup ./moniter.sh &
-nohup ./ana_moniter.sh &
-ps -ef | egrep "moniter.sh|ana_moniter.sh" | egrep -v egrep
+ps -ef | egrep "moniter.sh|MoniterStart" | egrep -v egrep
 
